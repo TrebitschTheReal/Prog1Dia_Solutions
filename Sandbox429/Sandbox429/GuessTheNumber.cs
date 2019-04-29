47d00983fb56cf92157432ec50791a17f734d633
Viktor Dohányos <vorkapp@gmail.com>
2019-04-29 23:04:19 +0200

    1

---
+++ Sandbox429/Sandbox429/GuessTheNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessGame
+{
+    class GuessTheNumber
+    {
+        private static Random r = new Random();
+        private int inputNumber;
+        private int generatedNumber = r.Next(1,11);
+        private string answer = "";
+
+        public int GeneratedNumber
+        {
+            get { return generatedNumber; }
+        }
+
+        public void InputNumber(int inputNumber)
+        {
+            this.inputNumber = inputNumber;
+        }
+        private string Umpire()
+        {
+            if (inputNumber > generatedNumber)
+            {
+                return "A számod NAGYOBB, mint a gondolt szám.";
+            }
+            else if(inputNumber < generatedNumber)
+            {
+                return "A számod KISEBB, mint a gondolt szám.";
+            }
+            return "You are a magician!";
+        }
+        public void Result()
+        {
+            Console.WriteLine(Umpire());
+        }
+
+
+    }
+}
---
+++ Sandbox429/Sandbox429/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sandbox429
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            var guessgame = new GuessTheNumber();
+            int inputNum;
+            do
+            {
+                inputNum = int.Parse(Console.ReadLine());
+                guessgame.InputNumber(inputNum);
+                guessgame.Result();
+            } while (inputNum != guessgame.GeneratedNumber);
+            Console.ReadKey();
+        }
+    }
+}
