e61710f5323d952d363da5c0eb4c0a14fa0d5b28
Viktor Dohányos <vorkapp@gmail.com>
2019-05-01 18:37:35 +0200

    1

---
+++ StringStuff/StringStuff/CreateStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringStuff
+{
+    class CreateStrings
+    {
+        private static Random r = new Random();
+        private string letters = "qwertzuiopasdfghjkl123456789xcvbnm";
+
+        public string CreateRandomName()
+        {
+            string generatedName = "";
+
+            do
+            {
+                generatedName += letters[r.Next(0, letters.Length)];
+
+            } while (generatedName.Length != 5);
+
+            generatedName += ".txt";
+            return generatedName;
+        }
+
+    }
+}
---
+++ StringStuff/StringStuff/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace StringStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringmaker = new CreateStrings();
+            int cnt = 0;
+
+            do
+            {
+                cnt++;
+                string nameOfTheTxt = stringmaker.CreateRandomName();
+                var sw = new StreamWriter(@"E:\maszkolgatunk\" + nameOfTheTxt);
+                sw.WriteLine("A nyuszi az jó!");
+                sw.Close();
+            } while (cnt != 20);
+        }
+    }
+}
