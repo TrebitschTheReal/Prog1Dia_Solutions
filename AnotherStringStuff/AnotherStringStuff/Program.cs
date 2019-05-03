90b89b6a9a923d7d966ccb0e80e4fa024bcec8eb
Viktor Dohányos <vorkapp@gmail.com>
2019-05-03 13:11:08 +0200

    11

---
+++ AnotherStringStuff/AnotherStringStuff/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AnotherStringStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var txtreader = new TxtReader();
+
+            Console.WriteLine(txtreader.FullText);
+            Console.WriteLine(txtreader.NumberOfLines);
+            Console.WriteLine(txtreader.NumberOfCharacters);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AnotherStringStuff/AnotherStringStuff/TxtReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AnotherStringStuff
+{
+    class TxtReader
+    {
+        private string fullText = "";
+        private int numberOfLines = 0;
+        private int numberOfLetters = 0;
+        private int numberOfNumbers = 0;
+        private int numberOfCharacters = 0;
+
+        public TxtReader()
+        {
+            this.fullText = GenerateFullText();
+            this.numberOfLines = LineCounter();
+            this.numberOfLetters = LetterCounter();
+            this.numberOfNumbers = NumberCounter();
+        }
+
+        private int LineCounter()
+        {
+            StreamReader sr = new StreamReader(@"E:\maszkolgatunk\rimworld.txt", Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+                sr.ReadLine();
+                numberOfLines++;
+            }
+            numberOCharacters = fullText.Length;
+            sr.Close();
+            return numberOfLines;
+        }
+        private int CharacterCounter()
+        {
+            StreamReader sr = new StreamReader(@"E:\maszkolgatunk\rimworld.txt", Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+                numberOfCharacters = fullText.Length;
+            }
+            sr.Close();
+            return numberOfCharacters;
+
+        }
+        private string GenerateFullText()
+        {
+            StreamReader sr = new StreamReader(@"E:\maszkolgatunk\rimworld.txt", Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+                string textPart = sr.ReadLine();
+                fullText += textPart;
+            }
+            sr.Close();
+            return fullText;
+        }
+
+        public string FullText
+        {
+            get { return fullText; }
+        }
+        public int NumberOfLines
+        {
+            get { return this.numberOfLines; }
+        }
+        public int NumberOfCharacters
+        {
+            get { return numberOfCharacters; }
+        }
+    }
+}
